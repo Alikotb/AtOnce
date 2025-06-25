aa44d5c3ea9e5f51d546877bdf19a353d6597d66
Ali Kotb <AliKotb38@gmail.com>
2025-06-25 11:38:46 +0300

    refactor handel internet status

--- app/src/main/java/com/example/atonce/presentation/MainActivity.kt
+++ app/src/main/java/com/example/atonce/presentation/MainActivity.kt
@@ -8,34 +8,30 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.atonce.core.enums.LanguageEnum
 import com.example.atonce.core.extensions.applyLanguage
-import com.example.atonce.domain.internet.ConnectivityObserver
 import com.example.atonce.domain.usecase.GetLanguageUseCase
-import com.example.atonce.presentation.common.navigation.SetUpNavHost
 import com.example.atonce.presentation.common.component.navigation.CustomBottomNavBar
+import com.example.atonce.presentation.common.navigation.SetUpNavHost
 import com.example.atonce.presentation.common.theme.AtOnceTheme
 import com.example.atonce.presentation.common.theme.DarkWhiteColor
 import com.example.atonce.presentation.common.theme.WhiteColor
-import com.example.atonce.presentation.no_internet.NoInternetScreen
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import org.koin.android.ext.android.inject
 
@@ -57,10 +53,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         val connectivityObserver: ConnectivityObserver by inject()
 
         setContent {
-            val isOnline by connectivityObserver.isOnline.observeAsState(initial = true)
             HideSystemUI()
             AtOnceTheme(darkTheme = isSystemInDarkTheme(), dynamicColor = false) {
                 navController = rememberNavController()
@@ -87,17 +81,13 @@ class MainActivity : ComponentActivity() {
                     }
                 ) { innerPadding ->
 
-                    if(isOnline) {
                         SetUpNavHost(
                             navController = navController,
                             bottomBarState = bottomBarState,
                             snackbarState = snackbarHostState,
                             paddingValues = innerPadding
                         )
-                    }else{
-                        bottomBarState.value=false
-                        NoInternetScreen()
-                    }
+
 
                 }
 
--- app/src/main/java/com/example/atonce/presentation/common/navigation/NavHost.kt
+++ app/src/main/java/com/example/atonce/presentation/common/navigation/NavHost.kt
@@ -5,10 +5,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
+import com.example.atonce.domain.internet.ConnectivityObserver
 import com.example.atonce.presentation.cart.view.CartScreen
 import com.example.atonce.presentation.home.view.HomeScreen
 import com.example.atonce.presentation.login.view.LoginScreen
@@ -21,6 +23,8 @@ import com.example.atonce.presentation.signup.SignUpScreen
 import com.example.atonce.presentation.splash.view.SplashScreen
 import com.example.atonce.presentation.store.view.StoreScreen
 import com.example.atonce.presentation.webview.WebViewScreen
+import androidx.compose.runtime.getValue
+import org.koin.compose.koinInject
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,11 +35,14 @@ fun SetUpNavHost(
     snackbarState: SnackbarHostState,
     paddingValues: PaddingValues
 ) {
+    val connectivityObserver: ConnectivityObserver = koinInject()
+    val isOnline by connectivityObserver.isOnline.observeAsState(initial = true)
     NavHost(
         navController = navController,
         startDestination = ScreenRoute.SplashScreen
 
     ) {
+
         composable<ScreenRoute.SplashScreen> {
             SplashScreen(
                 onNavToHome = {
@@ -54,89 +61,113 @@ fun SetUpNavHost(
         }
         composable<ScreenRoute.LoginScreen> {
             bottomBarState.value = false
-             LoginScreen(
-                 modifier =paddingValues,
-                 snackbarHostState = snackbarState,
-                 onRegisterClick = {
-                     navController.navigate(ScreenRoute.SignupScreen)
-                 },
-                 onLoginClick = {
-                     navController.navigate(ScreenRoute.HomeScreen) {
-                         popUpTo(0) { inclusive = true }
-                         launchSingleTop = true
-                     }
-                 }
-             )
-        }
-        composable<ScreenRoute.SignupScreen> {
-
-            bottomBarState.value = false
-            SignUpScreen(
-                modifier =paddingValues,
-                onBackClick = {
-                    navController.popBackStack()
-                },
+            LoginScreen(
+                modifier = paddingValues,
                 snackbarHostState = snackbarState,
                 onRegisterClick = {
-                    navController.navigate(ScreenRoute.LoginScreen) {
+                    navController.navigate(ScreenRoute.SignupScreen)
+                },
+                onLoginClick = {
+                    navController.navigate(ScreenRoute.HomeScreen) {
                         popUpTo(0) { inclusive = true }
                         launchSingleTop = true
                     }
                 }
             )
         }
+        composable<ScreenRoute.SignupScreen> {
+
+            bottomBarState.value = false
+            if (isOnline) {
+                SignUpScreen(
+                    modifier = paddingValues,
+                    onBackClick = {
+                        navController.popBackStack()
+                    },
+                    snackbarHostState = snackbarState,
+                    onRegisterClick = {
+                        navController.navigate(ScreenRoute.LoginScreen) {
+                            popUpTo(0) { inclusive = true }
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            } else {
+                NoInternetScreen()
+            }
+        }
         composable<ScreenRoute.HomeScreen> {
             bottomBarState.value = true
-            HomeScreen(
-                modifier =paddingValues,
-                onProfileClick = {
-                    navController.navigate(ScreenRoute.ProfileScreen)
-                },
-                onNavToStore = { warehouseId ->
-                    navController.navigate(ScreenRoute.StoreScreen(warehouseId))
-                }
-            )
+            if (isOnline) {
+                HomeScreen(
+                    modifier = paddingValues,
+                    onProfileClick = {
+                        navController.navigate(ScreenRoute.ProfileScreen)
+                    },
+                    onNavToStore = { warehouseId ->
+                        navController.navigate(ScreenRoute.StoreScreen(warehouseId))
+                    }
+                )
+            } else {
+                NoInternetScreen()
+            }
         }
         composable<ScreenRoute.StoreScreen> {
             bottomBarState.value = false
             val warehouseId = it.toRoute<ScreenRoute.StoreScreen>().warehouseId
-            StoreScreen(
-                warehouseId = warehouseId,
-                snackbarHostState = snackbarState,
-                modifier =paddingValues,
-                onBackClick = {
-                    navController.popBackStack()
-                }
-            )
+            if (isOnline) {
+                StoreScreen(
+                    warehouseId = warehouseId,
+                    snackbarHostState = snackbarState,
+                    modifier = paddingValues,
+                    onBackClick = {
+                        navController.popBackStack()
+                    }
+                )
+            } else {
+                NoInternetScreen()
+            }
         }
         composable<ScreenRoute.CartScreen> {
             bottomBarState.value = true
-            CartScreen(
-                modifier =paddingValues,
-                snackbarHostState = snackbarState
-            )
+            if (isOnline) {
+                CartScreen(
+                    modifier = paddingValues,
+                    snackbarHostState = snackbarState
+                )
+            } else {
+                NoInternetScreen()
+            }
         }
         composable<ScreenRoute.OrderScreen> {
             bottomBarState.value = true
-            OrderScreen(modifier =paddingValues)
+            if (isOnline) {
+                OrderScreen(modifier = paddingValues)
+            } else {
+                NoInternetScreen()
+            }
         }
         composable<ScreenRoute.SearchScreen> {
             bottomBarState.value = true
-            SearchScreen(
-                modifier =paddingValues,
-                snackbarHostState = snackbarState,
+            if (isOnline) {
+                SearchScreen(
+                    modifier = paddingValues,
+                    snackbarHostState = snackbarState,
                 )
+            } else {
+                NoInternetScreen()
+            }
         }
         composable<ScreenRoute.ProfileDetailsScreen> {
             bottomBarState.value = false
-            ProfileDetails( modifier =paddingValues,onClick={
+            ProfileDetails(modifier = paddingValues, onClick = {
                 navController.popBackStack()
             })
         }
         composable<ScreenRoute.ProfileScreen> {
             bottomBarState.value = false
             ProfileScreen(
-                modifier =paddingValues,
+                modifier = paddingValues,
                 onBackClick = {
                     navController.popBackStack()
                 },
@@ -154,15 +185,19 @@ fun SetUpNavHost(
         }
         composable<ScreenRoute.WebViewScreen> {
             bottomBarState.value = false
-            val title =it.toRoute<ScreenRoute.WebViewScreen>().title
+            val title = it.toRoute<ScreenRoute.WebViewScreen>().title
             val url = it.toRoute<ScreenRoute.WebViewScreen>().url
-            WebViewScreen(title = title, url = url, onBackClick = {
-                navController.popBackStack()
-            })
+            if (isOnline) {
+                WebViewScreen(title = title, url = url, onBackClick = {
+                    navController.popBackStack()
+                })
+            } else {
+                NoInternetScreen()
+            }
         }
         composable<ScreenRoute.NoInternetScreen> {
             bottomBarState.value = true
-            NoInternetScreen(padding=paddingValues)
+            NoInternetScreen(padding = paddingValues)
         }
     }
 }
