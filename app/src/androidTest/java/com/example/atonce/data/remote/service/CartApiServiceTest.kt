ef99f4757743fe197d62266439c64068a0712069
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2025-06-25 15:25:53 +0300

    feat: Add shimmer loading for pagination in SearchScreen
    This commit introduces a shimmer loading effect when fetching more items (pagination) in the `SearchScreen`.
    
    Key changes:
    
    - **SearchScreen.kt:**
        - Added `ShimmerSearchCard` to display as a placeholder while paginated items are loading.
        - Observes `isPaginationLoading` from the `SearchViewModel` to control the visibility of the shimmer effect.
    - **SearchViewModel.kt:**
        - Added `_isPaginationLoading` and `isPaginationLoading` state flows to track the pagination loading state.
        - Sets `_isPaginationLoading` to `true` before fetching paginated data and to `false` after the data is fetched or if an error occurs.

---
+++ app/src/androidTest/java/com/example/atonce/data/remote/service/CartApiServiceTest.kt
@@ -0,0 +1,65 @@
+package com.example.atonce.data.remote.service
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.atonce.data.remote.dto.cart.AddToCartRequestDto
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNull
+
+@RunWith(AndroidJUnit4::class)
+class CartApiServiceTest : KoinTest {
+
+    private val apiService : CartApiService by inject()
+
+    @Test
+    fun addToCart_wrongRequestParams_failure() {
+        runBlocking {
+            //given
+            val request = AddToCartRequestDto(
+                medicineId = -1,
+                pharmacyId = -1,
+                quantity = -1,
+                warehouseId = -1,
+                englishMedicineName = "",
+                arabicMedicineName = "",
+                medicineUrl = "",
+                warehouseUrl = "",
+                price = 0.0,
+                discount = 0.0,
+            )
+
+            //when
+            val response = apiService.addToCart(request)
+
+            //then
+            assertNotEquals(response.success, true)
+            assertEquals(response.message, "Medicine not found.")
+            assertNull(response.data)
+
+        }
+    }
+
+    @Test
+    fun addToCart_rightRequestParams_Success() {
+        runBlocking {
+            //given
+            val request = AddToCartRequestDto(
+                medicineId = 1,
+                pharmacyId = 1,
+                quantity = 1,
+                warehouseId = 1,
+                englishMedicineName = "",
+                arabicMedicineName = "",
+                medicineUrl = "",
+                warehouseUrl = "",
+                price = 0.0,
+                discount = 0.0,
+            )
+
+    }
+}
