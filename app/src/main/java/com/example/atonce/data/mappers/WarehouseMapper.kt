4c4f4a89148414c448385fbf3fd4e071edef93ee
Abdelrahman Kamel <126878089+Abdelrahman-Kamel8886@users.noreply.github.com>
2025-07-02 15:46:17 +0300

    Fix: Update default warehouse image URL
    The default image URL for warehouses in `WarehouseMapper.kt` has been changed from a Reuters image to an image hosted on `ibb.co`. The previous URL is now commented out.

--- app/src/main/java/com/example/atonce/data/mappers/WarehouseMapper.kt
+++ app/src/main/java/com/example/atonce/data/mappers/WarehouseMapper.kt
@@ -10,7 +10,8 @@ fun WarehouseDto.toEntity() : Warehouse {
         name = name,
         address = address,
         governate = governate,
-        imageUrl =  "https://www.reuters.com/resizer/v2/QOZLON6ZHRNQFO36VAFQCB2ZI4.jpg?auth=d82124e2af1cdaba30a06144e7637e34e99e770d04320ffe7b8a208eac3e379c&width=1080&quality=80",
+        imageUrl =  "https://i.ibb.co/MjjRxG4/2-2025-12-44-52.png",
+       // imageUrl =  "https://www.reuters.com/resizer/v2/QOZLON6ZHRNQFO36VAFQCB2ZI4.jpg?auth=d82124e2af1cdaba30a06144e7637e34e99e770d04320ffe7b8a208eac3e379c&width=1080&quality=80",
         minmumPrice = minmumPrice,
         deliveryRate = deliveryRate ?: "Daily"
     )
